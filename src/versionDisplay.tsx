b00698851d913dff75791fededf817749d51ca04
Ido Nava <idonava@gmail.com>
2024-07-31 10:18:07 +0300

    initial commit

---
+++ src/versionDisplay.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const packageVersion = "1.0.0";
+
+const VersionDisplay = () => {
+  return <div>react-version-display Version: {packageVersion}</div>;
+};
+
+export default VersionDisplay;
