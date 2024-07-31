634901328712c30ffe3c281bee860ce7de32b128
Ido Nava <idonava@gmail.com>
2024-07-31 10:59:00 +0300

    v1.0.1

--- src/versionDisplay.tsx
+++ src/versionDisplay.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const packageVersion = "1.0.0";
+export const packageVersion = "1.0.1";
 
 export const VersionDisplay = () => {
   return <div>external-package-version-display Version: {packageVersion}</div>;
