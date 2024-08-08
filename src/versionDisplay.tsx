3eae1c28de415ab47a62f1623ab409262d8e6213
Ido Nava <idonava@gmail.com>
2024-08-08 13:54:26 +0300

    v2.1.4

--- src/versionDisplay.tsx
+++ src/versionDisplay.tsx
@@ -1,8 +1,20 @@
 import { Box } from "@mui/material";
+import {
+  VersionDisplay as VersionDisplay2,
+  packageVersion as packageVersion2,
+} from "external-package-version-display-2";
 import React from "react";
 
-export const packageVersion = "2.1.3";
+export const packageVersion = "2.1.4";
+export const packageVersionOfExternalPackage2 = packageVersion2;
 
 export const VersionDisplay = () => {
-  return <Box>external-package-version-display Version: {packageVersion}</Box>;
+  return (
+    <Box sx={{ backgroundColor: "yellow" }}>
+      <Box>external-package-version-display Version: {packageVersion}</Box>
+      <Box>
+        external-package-version-display2 Version: <VersionDisplay2 />
+      </Box>
+    </Box>
+  );
 };
